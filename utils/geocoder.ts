813c695a094f868d6177d2766238e96728ed13d1
ABelli99 <andreabelli1999@gmail.com>
2024-04-15 10:38:36 +0200

    geocoder js2ts

---
+++ utils/geocoder.ts
@@ -0,0 +1,12 @@
+import NodeGeocoder, { GeocoderOptions, Geocoder } from 'node-geocoder';
+
+const options: GeocoderOptions = {
+    provider: process.env.GEOCODER_PROVIDER,
+    httpAdapter: 'https',
+    apiKey: process.env.GEOCODER_API_KEY,
+    formatter: null
+}
+
+const geocoder: Geocoder = NodeGeocoder(options);
+
+export default geocoder;
