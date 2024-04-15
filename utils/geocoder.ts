98c5e96e29835347fab951828f4286461b85a32f
ABelli99 <andreabelli1999@gmail.com>
2024-04-15 12:05:17 +0200

    geocoder js2ts

--- utils/geocoder.ts
+++ utils/geocoder.ts
@@ -1,8 +1,8 @@
-import NodeGeocoder, { GeocoderOptions, Geocoder } from 'node-geocoder';
+import NodeGeocoder, { Geocoder } from 'node-geocoder';
 
-const options: GeocoderOptions = {
-    provider: process.env.GEOCODER_PROVIDER,
-    httpAdapter: 'https',
+const options: NodeGeocoder.Options = {
+    provider: process.env.GEOCODER_PROVIDER as NodeGeocoder.Providers,
+    //httpAdapter: 'https',             // didn't found any info regarding this missing variable
     apiKey: process.env.GEOCODER_API_KEY,
     formatter: null
 }
