142440e08bb5f7fe40c467351d502a96863f83d9
ABelli99 <andreabelli1999@gmail.com>
2024-04-16 11:20:01 +0200

    fixed a typo

--- middleware/advancedResults.ts
+++ middleware/advancedResults.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-const advancedResults = (model: any, populate: any) => async (req: Request, res: Response, next: NextFunction) => {
+const advancedResults = (model: any, populate?: any) => async (req: Request, res: Response, next: NextFunction) => {
   let query: any;
 
   // Copy req.query
